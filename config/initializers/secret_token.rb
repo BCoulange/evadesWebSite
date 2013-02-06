080d2c96c40d15f5ff748046ad4662d041fad65e
Baptiste Coulange <bc@cornis.fr>
2013-02-06 22:08:35 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EvadesWebSite::Application.config.secret_token = '10d3ae88e053884d420d534a4553a8c4b81c3af9e2c8a92a764c15ffc14e7c734923934bc0c74c9ff0f13adc23a2c8c13a6c86f974b9387c89aa01204831549f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EvadesWebSite::Application.config.session_store :cookie_store, key: '_evadesWebSite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EvadesWebSite::Application.config.session_store :active_record_store
