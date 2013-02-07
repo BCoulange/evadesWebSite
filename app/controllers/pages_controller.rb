2c71a8fcaa716ef941ed1b9715e7e9042ad462ae
Baptiste Coulange <bc@cornis.fr>
2013-02-07 10:25:36 +0100

    changement du choix du der spectacle

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     @spectacles=Spectacle.all
+    @prochain_s=@spectacles.sort_by{|s| s.start_time}.reject! { |s| s.start_time-DateTime.now<0 }[0]
     render :layout => 'homepage'
   end
 
