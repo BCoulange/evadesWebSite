ff6835f66abe7d7d5dfa5e22583c9076808a3bba
Baptiste Coulange <bc@cornis.fr>
2013-02-07 01:04:53 +0100

    ajout de relation entre lieu et spectacle (marchepo)

--- app/models/lieu.rb
+++ app/models/lieu.rb
@@ -1,3 +1,4 @@
 class Lieu < ActiveRecord::Base
   attr_accessible :adresse, :gMapUrl, :nom
+  has_many :spectacles
 end
--- app/models/spectacle.rb
+++ app/models/spectacle.rb
@@ -1,3 +1,4 @@
 class Spectacle < ActiveRecord::Base
   attr_accessible :message, :titre, :start_time
+  belongs_to :lieu
 end
