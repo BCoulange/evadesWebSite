24c9cc81bd3162f4e2bb93b87273ba689204f9ff
Baptiste Coulange <bc@cornis.fr>
2013-02-07 11:11:17 +0100

    suppression de l'email du model user pour préparer l'integration de devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130207093817) do
+ActiveRecord::Schema.define(:version => 20130207100819) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -74,7 +74,6 @@ ActiveRecord::Schema.define(:version => 20130207093817) do
     t.string   "prenom"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -86,7 +85,6 @@ ActiveRecord::Schema.define(:version => 20130207093817) do
     t.string   "last_sign_in_ip"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
