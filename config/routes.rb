39ffb7e734c341c0b5056d32c9d9a92df8054199
Baptiste Coulange <bc@cornis.fr>
2013-02-13 14:14:13 +0100

    dégagage de la page new

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ EvadesWebSite::Application.routes.draw do
 
 
 
-  resources :mailings
+  resources :mailings, :except => :new
 
 
   devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
