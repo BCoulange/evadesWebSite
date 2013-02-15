ec2afce4f0c6db0f80558139e3111e17015cca6b
Baptiste Coulange <bc@cornis.fr>
2013-02-15 17:49:33 +0100

    helper de menu su

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all.shuffle
 
     respond_to do |format|
       format.html # index.html.erb
